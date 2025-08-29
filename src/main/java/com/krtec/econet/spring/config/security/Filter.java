a85f0228850ce59c16adaba85e64e3bc98680be6
Ricardo Pereira <ricardo@krinfo.com.br>
2025-08-29 15:49:55 -0300

    banco de dados em memoria, h2

--- src/main/java/com/krtec/econet/spring/config/security/Filter.java
+++ src/main/java/com/krtec/econet/spring/config/security/Filter.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class Filter {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
@@ -17,9 +18,10 @@ public class Filter {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/public/**").permitAll()
-                        .anyRequest()
-                        .authenticated()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
                 )
+                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // 🔑 precisa disso
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(((request, response, authException) ->
                                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
